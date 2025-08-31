63084182887be2fc5a32fb39d6f172a407b0c498
r-uu <roger.wegner@web.de>
2025-08-31 22:00:24 +0200

    get rid of target in remote repository

--- r-uu/app/jeeeraaah/common/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_TaskGroup_Lazy_BeanImpl.java
+++ r-uu/app/jeeeraaah/common/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_TaskGroup_Lazy_BeanImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-31T21:58:25+0200",
+    date = "2025-08-31T21:59:43+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 public class Map_TaskGroup_Lazy_BeanImpl implements Map_TaskGroup_Lazy_Bean {
