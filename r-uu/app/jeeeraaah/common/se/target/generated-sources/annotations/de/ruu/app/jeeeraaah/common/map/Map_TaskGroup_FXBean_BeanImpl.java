fdd19d90c7760f286f45348bb12fcc04c654561c
r-uu <roger.wegner@web.de>
2025-09-09 08:16:56 +0200

    in progress

--- r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_TaskGroup_FXBean_BeanImpl.java
+++ r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_TaskGroup_FXBean_BeanImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-07T07:13:09+0200",
+    date = "2025-09-09T07:25:59+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 public class Map_TaskGroup_FXBean_BeanImpl implements Map_TaskGroup_FXBean_Bean {
--- r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_Task_EntityJPA_LazyImpl.java
+++ r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_Task_EntityJPA_LazyImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-07T07:13:08+0200",
+    date = "2025-09-09T07:26:00+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 public class Map_Task_EntityJPA_LazyImpl implements Map_Task_EntityJPA_Lazy {
