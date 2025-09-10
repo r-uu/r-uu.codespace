062af89bbce0f9b98da350bd8a13c2452378fa6b
r-uu <roger.wegner@web.de>
2025-09-10 08:40:03 +0200

    removed jacoco from build steps because it fails due to java class file version 24

--- r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_Task_EntityJPA_EntityDTOImpl.java
+++ r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_Task_EntityJPA_EntityDTOImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-09T22:45:45+0200",
+    date = "2025-09-10T08:38:48+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 public class Map_Task_EntityJPA_EntityDTOImpl implements Map_Task_EntityJPA_EntityDTO {
