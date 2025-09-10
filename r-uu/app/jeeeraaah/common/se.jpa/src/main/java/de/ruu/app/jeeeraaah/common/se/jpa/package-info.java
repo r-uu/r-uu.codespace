5f53c58b5b14858f4c5c732b354ed2126970f728
r-uu <roger.wegner@web.de>
2025-09-10 11:00:57 +0200

    Delete r-uu/app/jeeeraaah/common/se.jpa directory

--- r-uu/app/jeeeraaah/common/se.jpa/src/main/java/de/ruu/app/jeeeraaah/common/se/jpa/package-info.java
+++
@@ -1,6 +0,0 @@
-package de.ruu.app.jeeeraaah.common.se.jpa;
-
-/**
- * Moved this package to module de.ruu.app.jeeeraaah.common.jpa due to severe problems using the entity types defined
- * there from this module. Currently, there is no workaround for this problem.
- */
