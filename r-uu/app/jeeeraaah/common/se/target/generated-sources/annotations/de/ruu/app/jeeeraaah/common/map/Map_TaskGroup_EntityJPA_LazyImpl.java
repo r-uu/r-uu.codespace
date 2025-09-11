aa5205b4d951816dcc6c5c69f426e1736b22be6f
r-uu <roger.wegner@web.de>
2025-09-11 07:45:26 +0200

    in progress

--- r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_TaskGroup_EntityJPA_LazyImpl.java
+++ r-uu/app/jeeeraaah/common/se/target/generated-sources/annotations/de/ruu/app/jeeeraaah/common/map/Map_TaskGroup_EntityJPA_LazyImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-10T17:45:32+0200",
+    date = "2025-09-11T07:32:39+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 public class Map_TaskGroup_EntityJPA_LazyImpl implements Map_TaskGroup_EntityJPA_Lazy {
--- r-uu/lib/jpa/core.mapstruct.demo.bidirectional/target/generated-sources/annotations/de/ruu/lib/jpa/core/mapstruct/demo/tree/MapperNodeSimpleNodeEntityImpl.java
+++ r-uu/lib/jpa/core.mapstruct.demo.bidirectional/target/generated-sources/annotations/de/ruu/lib/jpa/core/mapstruct/demo/tree/MapperNodeSimpleNodeEntityImpl.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-10T17:45:31+0200",
+    date = "2025-09-11T07:33:56+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 class MapperNodeSimpleNodeEntityImpl extends MapperNodeSimpleNodeEntity {
