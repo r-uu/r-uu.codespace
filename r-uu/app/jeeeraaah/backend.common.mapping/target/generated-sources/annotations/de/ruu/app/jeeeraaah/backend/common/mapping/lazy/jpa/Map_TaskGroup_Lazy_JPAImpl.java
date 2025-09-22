7ea85147a12eb359269ce560136421e634df2cd8
r-uu <roger.wegner@web.de>
2025-09-22 17:41:52 +0200

    in progress

---
+++ r-uu/app/jeeeraaah/backend.common.mapping/target/generated-sources/annotations/de/ruu/app/jeeeraaah/backend/common/mapping/lazy/jpa/Map_TaskGroup_Lazy_JPAImpl.java
@@ -0,0 +1,29 @@
+package de.ruu.app.jeeeraaah.backend.common.mapping.lazy.jpa;
+
+import de.ruu.app.jeeeraaah.backend.persistence.jpa.TaskGroupJPA;
+import de.ruu.app.jeeeraaah.common.api.domain.TaskGroupLazy;
+
+/*
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-22T17:40:57+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
+)
+*/
+public class Map_TaskGroup_Lazy_JPAImpl implements Map_TaskGroup_Lazy_JPA {
+
+    @Override
+    public TaskGroupJPA map(TaskGroupLazy in) {
+        if ( in == null ) {
+            return null;
+        }
+
+        TaskGroupJPA taskGroupJPA = create( in );
+
+        beforeMapping( in, taskGroupJPA );
+
+        afterMapping( in, taskGroupJPA );
+
+        return taskGroupJPA;
+    }
+}
---
+++ r-uu/app/jeeeraaah/backend.common.mapping/target/generated-sources/annotations/de/ruu/app/jeeeraaah/backend/common/mapping/lazy/jpa/Map_Task_Lazy_JPAImpl.java
@@ -0,0 +1,31 @@
+package de.ruu.app.jeeeraaah.backend.common.mapping.lazy.jpa;
+
+import de.ruu.app.jeeeraaah.backend.persistence.jpa.TaskGroupJPA;
+import de.ruu.app.jeeeraaah.backend.persistence.jpa.TaskJPA;
+import de.ruu.app.jeeeraaah.common.api.domain.TaskLazy;
+
+/*
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-22T17:40:57+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
+)
+*/
+public class Map_Task_Lazy_JPAImpl implements Map_Task_Lazy_JPA {
+
+    @Override
+    public TaskJPA map(TaskGroupJPA group, TaskLazy in) {
+        if ( group == null && in == null ) {
+            return null;
+        }
+
+        TaskJPA taskJPA = create( group, in );
+
+        if ( group != null ) {
+            taskJPA.setName( group.getName() );
+            taskJPA.description( group.getDescription() );
+        }
+
+        return taskJPA;
+    }
+}
